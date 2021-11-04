e9162c4007ef69486901f57b806262f8ceb1f389
maxdevsr <macson_sr@hotmail.com>
2021-11-04 13:21:28 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
@@ -1,26 +1,27 @@
 package com.devsuperior.dsvendas.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsvendas.dto.SellerDTO;
-import com.devsuperior.dsvendas.service.SellerService;
+import com.devsuperior.dsvendas.entities.Seller;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
 
 @RestController
 @RequestMapping(value = "/sellers")
 public class SellerController {
 	@Autowired
-	private SellerService service;
+    private SellerRepository repository;
 	
 	@GetMapping
-	public ResponseEntity<List<SellerDTO>> findAll() {
-		List<SellerDTO> list = service.findAll();
-		return ResponseEntity.ok(list);	
-	}
+	public List<SellerDTO> findAll(){
+        List<Seller> list = repository.findAll();
+        return list.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+    }
 
 }
